009b6ac35fa7317cada15eec6d4c02e4bb15d1ab
Mike <mpawlak719@gmail.com>
2022-05-27 16:17:13 -0500

    try to automate the index fix

--- build_scripts/predeploy.js
+++ build_scripts/predeploy.js
@@ -7,10 +7,6 @@ const fs = require("fs");
     // eslint-disable-next-line no-console
     console.log("Building action started...");
     await execa("npm", ["run", "build"]);
-    // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
-    await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log(e.message);
